1c651f004de89392ae7840eee17be1bf056bc1cd
Sanketh Katta <sankethkatta@gmail.com>
2012-05-16 14:27:58 -0700

    fixing variable port number

--- app.js
+++ app.js
@@ -186,5 +186,6 @@ app.post('/registerLogin', function(req,res){
   }
 });
 
-app.listen(5000);
+var port = process.env.PORT || 3000
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
